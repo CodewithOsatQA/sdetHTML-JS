1c2608f8a4db27305732b5f246ed821077545a44
vinesh <vineshghela@gmail.com>
2020-09-23 09:54:29 +0100

    html and js files added

---
+++ JavaScript/intro/javascript/main.js
@@ -0,0 +1,36 @@
+console.log("Hello friends")
+
+// single comment
+/*
+multi
+line 
+comment
+*/
+
+// ECMA6 - this is latest standard 
+// let - cna be changed over and over
+// const - const value that once set cannot be changed 
+
+
+let myFirstLet;
+myFirstLet = "Hello everyone";
+
+console.log(myFirstLet)
+console.info(myFirstLet)
+console.warn(myFirstLet)
+console.error(myFirstLet)
+
+let myTxt = "This could be useful!";// string
+let myNum = 23456; // Number
+let myBool = true; // boolean
+let myUndef; //undefined
+let myNull = null; // Null
+let myBigInt = 3234567898765543546877697; // Big int
+//symbol
+//Object
+
+console.log(myTxt+"this is my string");
+console.log(`this is my let variable ${myTxt} then i can keep typing`);
+
+
+
